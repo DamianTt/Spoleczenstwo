77cd6f828890ef50e1836ace393f194a88689ca1
tdec <11tomdec@gmail.com>
2016-04-10 22:40:26 +0200

    Change user registration
     -Add user name
     -Make password easier

--- SI/Models/AccountViewModels.cs
+++ SI/Models/AccountViewModels.cs
@@ -65,12 +65,16 @@ namespace SI.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
